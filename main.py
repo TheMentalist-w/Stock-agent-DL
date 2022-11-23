d08d172c5c62883be623037245b4481ee425f149
szymon8576 <50836217+szymon8576@users.noreply.github.com>
2022-11-23 22:11:26 +0100

    data preprocessing - loading, joining and pruning dataframes

---
+++ main.py
@@ -0,0 +1,24 @@
+import dataPreprocessing as dpp
+from matplotlib import pyplot as plt
+
+
+if __name__ == '__main__':
+
+    dataframes = dpp.load_data("./data")    # load data from ./data directory
+    matrix = dpp.join_dataframes(dataframes)  # join dataframes
+
+    # drop NaN values.
+    # ,,all" - drop rows that consist of _only_ Nan values
+    # ,,any" - drop rows that have _at least_ one Nan value
+    matrix = matrix.dropna(how="any")
+
+    # print & plot some data!
+    print(matrix)
+
+    plt.figure(figsize=(15, 10), dpi=80)
+
+    for col in matrix.columns:
+        plt.semilogy(matrix.index, matrix[col], label=col)
+
+    plt.legend()
+    plt.savefig("example.png")
